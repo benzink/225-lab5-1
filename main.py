abec1fedd01c620f36b90fa5800b332ac9e56d54
benzink <benzink@miamioh.edu>
2025-11-06 16:25:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-from flask import Flask, render_template
-import sqlite3, os
+from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
-DB_PATH = os.path.join("data", "sample_data.db")
-
 def init_db():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect('data/data.db')
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)')
     conn.commit()
     conn.close()
 
 @app.route('/')
 def home():
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    c.execute("SELECT name FROM items")
-    items = [row[0] for row in c.fetchall()]
-    conn.close()
-    return render_template("index.html", items=items)
+    return "Hello from Flask CI/CD!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     init_db()
     app.run(host='0.0.0.0', port=5000)
